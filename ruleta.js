db923651a4968606f45d79f739db1a4e398d6ddb
avacy04-code <a.vacy04@gmail.com>
2026-03-10 17:44:57 +0100

    Add challenge spinner functionality in ruleta.js

---
+++ ruleta.js
@@ -0,0 +1,27 @@
+let retos=[
+
+"20 jumping jacks",
+
+"15 sentadillas",
+
+"10 burpees",
+
+"30 segundos plancha",
+
+"carrera alrededor del gimnasio",
+
+"equilibrio 20 segundos",
+
+"reto cooperativo con balón"
+
+]
+
+function girarRuleta(){
+
+let n=Math.floor(Math.random()*retos.length)
+
+document.getElementById("reto").innerText="🎯 RETO: "+retos[n]
+
+sonidoRetoActivo()
+
+}
