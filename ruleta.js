21dbc9553ba7edd2b1ca492f746a4e8bfac1cf19
avacy04-code <a.vacy04@gmail.com>
2026-03-11 23:37:28 +0100

    Update ruleta.js

--- ruleta.js
+++ ruleta.js
@@ -1,23 +1,4 @@
-let retos=[
-
-"10 burpees",
-"15 sentadillas",
-"30 segundos plancha",
-"20 jumping jacks",
-"equilibrio 20 segundos",
-"carrera hasta la pared y volver",
-"10 flexiones",
-"20 saltos de comba"
-
-]
-
-let girando=false
-
 function girarRuleta(){
-
-if(girando) return
-
-girando=true
 
 let ruleta=document.getElementById("ruleta")
 
@@ -32,8 +13,6 @@ let sector=Math.floor((grados%360)/45)
 let reto=retos[sector]
 
 document.getElementById("reto").innerText="🎯 RETO: "+reto
-
-girando=false
 
 },4000)
 
