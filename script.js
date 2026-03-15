5fd74a8456aaa634d02602fddbcca9bef859bbc4
avacy04-code <a.vacy04@gmail.com>
2026-03-15 17:51:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,26 @@
-// VARIABLES DEL JUEGO
-
 let actual = 0
 let puntos = 0
-let contador
+let contador = null
 let equipoSeleccionado = null
-
-
-
-// SELECCIONAR EQUIPO
-
-function seleccionarEquipo(i){
-
-equipoSeleccionado = i
-
-alert("Equipo seleccionado: " + equipos[i].nombre)
-
-actualizarRanking()
-
-}
-
-
-
-// MOSTRAR PREGUNTA
+let insigniasGanadas = []
 
 function mostrarPregunta(){
 
 if(actual >= preguntas.length){
-
 mostrarPodio()
-
 return
-
 }
 
 let p = preguntas[actual]
 
 document.getElementById("pregunta").innerText = p.pregunta
 
-for(let i=0;i<4;i++){
-
-document.getElementById("r"+i).innerText = p.respuestas[i]
-
-}
+p.respuestas.forEach((r,i)=>{
+document.getElementById("r"+i).innerText = r
+})
 
 iniciarTiempo()
-
 }
-
-
-
-// RESPONDER
 
 function responder(opcion){
 
@@ -61,39 +32,23 @@ if(opcion === p.correcta){
 
 puntos += 10
 
-if(equipoSeleccionado !== null){
-
-equipos[equipoSeleccionado].puntos += 10
-
-}
-
-if(typeof sonidoAcierto === "function") sonidoAcierto()
+sonidoAcierto()
 
 comprobarInsignias()
 
 }else{
 
-if(typeof sonidoError === "function") sonidoError()
+sonidoError()
 
 }
 
 document.getElementById("puntos").innerText = puntos
 
-actualizarRanking()
-
 actual++
 
-setTimeout(()=>{
-
-mostrarPregunta()
-
-},2000)
+setTimeout(mostrarPregunta,1500)
 
 }
-
-
-
-// TEMPORIZADOR CIRCULAR
 
 function iniciarTiempo(){
 
@@ -123,74 +78,70 @@ mostrarPregunta()
 
 }
 
-
-
-// INSIGNIAS SALUDABLES
-
 function comprobarInsignias(){
 
-if(puntos >= 30){
+const logros = [
+{p:30,t:"🥗 Experto saludable"},
+{p:60,t:"🏃 Maestro del movimiento"},
+{p:100,t:"🏆 Super atleta"}
+]
 
-document.getElementById("insignias").innerHTML += "🥗 Experto saludable<br>"
-
+logros.forEach(l=>{
+if(puntos >= l.p && !insigniasGanadas.includes(l.t)){
+insigniasGanadas.push(l.t)
+document.getElementById("insignias").innerHTML += l.t+"<br>"
 }
-
-if(puntos >= 60){
-
-document.getElementById("insignias").innerHTML += "🏃 Maestro del movimiento<br>"
-
-}
-
-if(puntos >= 100){
-
-document.getElementById("insignias").innerHTML += "🏆 Super atleta saludable<br>"
-
-}
-
-}
-
-
-
-// RANKING EN VIVO
-
-function actualizarRanking(){
-
-equipos.sort((a,b)=>b.puntos-a.puntos)
-
-let html = "🏆 Ranking<br>"
-
-equipos.forEach((e,i)=>{
-
-html += (i+1) + ". " + e.nombre + " - " + e.puntos + " pts<br>"
-
 })
 
-document.getElementById("ranking").innerHTML = html
-
 }
-
-
-
-// PODIO FINAL
 
 function mostrarPodio(){
 
-equipos.sort((a,b)=>b.puntos-a.puntos)
+document.getElementById("pregunta").innerHTML =
+"🏆 Juego terminado<br>Puntos: "+puntos
 
-let html = "<h2>🏆 PODIO FINAL</h2>"
-
-html += "🥇 " + equipos[0].nombre + " - " + equipos[0].puntos + " pts<br>"
-html += "🥈 " + equipos[1].nombre + " - " + equipos[1].puntos + " pts<br>"
-html += "🥉 " + equipos[2].nombre + " - " + equipos[2].puntos + " pts<br>"
-
-document.getElementById("pregunta").innerHTML = html
-
-if(typeof sonidoVictoria === "function") sonidoVictoria()
+sonidoVictoria()
 
 }
 
-
-
-// INICIAR JUEGO
-
 mostrarPregunta()
+
+// RULETA
+
+let categorias = [
+"Salud",
+"Deporte",
+"Alimentación",
+"Cuerpo humano"
+]
+
+function girarRuleta(){
+
+let i = Math.floor(Math.random()*categorias.length)
+
+let categoria = categorias[i]
+
+document.getElementById("categoria").innerText =
+"🎡 Categoría: "+categoria
+
+ruletaSound.play()
+
+}
+
+// MISIONES
+
+let misiones = [
+"Salta 3 veces",
+"Di una fruta",
+"Imita un animal",
+"Corre en el sitio"
+]
+
+function mostrarMision(){
+
+let m = misiones[Math.floor(Math.random()*misiones.length)]
+
+document.getElementById("mision").innerText =
+"🎯 Misión: "+m
+
+}
