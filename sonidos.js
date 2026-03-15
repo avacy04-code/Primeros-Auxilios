562cb869ffebca3d98eba75316d7a5ab9578ff48
avacy04-code <a.vacy04@gmail.com>
2026-03-15 17:53:09 +0100

    Update sonidos.js

--- sonidos.js
+++ sonidos.js
@@ -1,7 +1,16 @@
-let correcto=new Audio("sonidos/correcto.mp3")
-let incorrecto=new Audio("sonidos/incorrecto.mp3")
-let ruletaSound=new Audio("sonidos/ruleta.mp3")
-let victoria=new Audio("sonidos/victoria.mp3")
+let correcto = new Audio("sonidos/correcto.mp3")
+let incorrecto = new Audio("sonidos/incorrecto.mp3")
+let ruletaSound = new Audio("sonidos/ruleta.mp3")
+let victoria = new Audio("sonidos/victoria.mp3")
 
-function sonidoAcierto(){correcto.play()}
-function sonidoError(){incorrecto.play()}
+function sonidoAcierto(){
+correcto.play()
+}
+
+function sonidoError(){
+incorrecto.play()
+}
+
+function sonidoVictoria(){
+victoria.play()
+}
